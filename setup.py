8b7beed349052465295fcd99d817b47c02f07da6
github_username <rsingh15_be22@thapar.edu>
2025-01-19 23:09:11 +0530

    changes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Topsis-102216005",  # Replace with your package name
-    version="1.0.2",
+    name="102216005_Topsis", 
+    version="1.0.1",
     author="Reetinder Singh",
     author_email="your.email@example.com",
     description="A Python implementation of the TOPSIS decision-making method",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Reetinder932/topsis",  # Replace with your GitHub URL
+    url="https://github.com/Reetinder932/topsis",  
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
