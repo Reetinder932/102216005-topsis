bd6ef5a45bd47c2906a1a84065fe14df127052a6
github_username <rsingh15_be22@thapar.edu>
2025-01-19 22:28:29 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-102216005",  # Replace with your package name
+    version="1.0.2",
+    author="Reetinder Singh",
+    author_email="your.email@example.com",
+    description="A Python implementation of the TOPSIS decision-making method",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Reetinder932/topsis",  # Replace with your GitHub URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main",  # Expose the CLI command
+        ],
+    },
+    python_requires=">=3.6",
+)
